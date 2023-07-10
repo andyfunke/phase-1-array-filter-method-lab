7036d834a2d6c67e1bf98962c4116f6a93df864d
Andrew <andrew.i.funke@gmail.com>
2023-07-10 09:46:50 -0700

    Array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+// const drivers = [{name:'Bobby'}, {name:'Sammy'}, {name:'Sally'}, {name:'Annette'}, {name:'Sarah'}, {name:'bobby'}]
+// const drivers = ['Bobby','Sammy','Sally','Annette','Sarah','bobby']
+
+function findMatching(drivers, driver){
+    const returnValue = drivers.filter(drivers => drivers.toLowerCase() === driver.toLowerCase());
+    return returnValue;
+    }
+
+function fuzzyMatch(drivers, driver){
+    const returnValue = drivers.filter(drivers => drivers[0] === driver[0]);
+
+    return returnValue;
+        }
+
+function matchName(drivers, driver){
+    const returnValue = drivers.filter(drivers => drivers.name === driver);
+
+    return returnValue;
+}
+    
+matchName(drivers,'Bobby');
+
+fuzzyMatch(drivers,'Bobby');
+
+findMatching(drivers,'Bobby');
